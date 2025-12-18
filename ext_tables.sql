3facbe205ac3a02b33e87d1441250735b3458a92
Ernedin Zajko <ezajko@root.ba>
2025-12-18 13:31:01 +0100

    Remove ext_tables.sql in favor of TCA auto-schema

--- ext_tables.sql
+++
@@ -1,7 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-    frame_layout varchar(255) DEFAULT 'default' NOT NULL,
-    tx_spark_debug int(11) unsigned DEFAULT '0' NOT NULL
-);
